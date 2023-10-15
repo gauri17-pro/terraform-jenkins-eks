d60a1ea0b189da844e030162f522091976955b83
gauri <gaurishirkande11111@gmail.com>
2023-10-15 21:40:45 +0530

    Adding Cluster Endpoint Access

--- EKS/main.tf
+++ EKS/main.tf
@@ -35,6 +35,8 @@ module "eks" {
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.24"
 
+  cluster_endpoint_public_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
